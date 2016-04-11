a2ed2b79ad2efdd536afc9ae46b87b5010e415f5
Bec Todd <bec@bectodd.com>
2016-04-12 00:17:37 +1000

    TC-16 Create Additional Email UI
    Update UI for secondary email based on
    https://projects.invisionapp.com/d/main#/console/4365594/146968746/previ
    ew

--- views/customs/changeEmail_success.php
+++ views/customs/changeEmail_success.php
@@ -1,5 +1,5 @@
 <div class="panel-heading">
-    <?php echo Yii::t('UserModule.views_account_changeEmail_success', '<strong>Change</strong> E-mail'); ?>
+    <?php echo Yii::t('UserModule.views_account_changeEmail_success', '<strong>Update</strong> Primary E-mail'); ?>
 </div>
 <div class="panel-body">
     <?php echo Yii::t('UserModule.views_account_changeEmail_success', 'We´ve just sent an confirmation e-mail to your new address. <br /> Please follow the instructions inside.'); ?>
