6c060355467ae76fb5c2cacf1da172a852f01c35
Den Volkov <volkovdenya@gmail.com>
2016-04-29 16:11:43 +0300

    TC-16 fix success page

--- views/customs/changeSeconderyEmail_success.php
+++ views/customs/changeSeconderyEmail_success.php
@@ -2,7 +2,7 @@
     <?php echo Yii::t('UserModule.views_account_changeEmail_success', '<strong>Update</strong> Secondary E-mail'); ?>
 </div>
 <div class="panel-body">
-    <?php echo Yii::t('UserModule.views_account_changeEmail_success', 'Success'); ?>
+    <?php echo Yii::t('UserModule.views_account_changeEmail_success', 'Success, a secondry email address has been added to your account'); ?>
 </div>
 
 
