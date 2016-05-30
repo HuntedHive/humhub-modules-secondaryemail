1bce6f857525ae163d06f5eecec3ef3ca0b3e301
Den Volkov <volkovdenya@gmail.com>
2016-05-30 17:21:48 +0300

    TC-22 - fix css style

--- widgets/views/emailNotifications.php
+++ widgets/views/emailNotifications.php
@@ -5,7 +5,11 @@
     $cs = Yii::app()->getClientScript();
     $cs->registerScriptFile("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
 ?>
-
+<style>
+    .topbar {
+        margin-top: 51px; //need if alert not close
+    }
+</style>
 <div class="alert alert-info alert-message text-center">
     <center>Hi <?=  Yii::app()->user->getModel()->username; ?>, don't forget to <a href="<?= Yii::app()->createUrl('user/account/changeEmail') ?>">add a secondary email address</a> to your account to retain access to TeachConnect when your institutional email address expires.</center>
     <button type="button" class="close"><i class="fa fa-close"></i></button>    
