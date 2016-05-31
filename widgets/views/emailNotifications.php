f2c6542382999eddb9267ddab1eb0841e7b70742
Bec Todd <bec@bectodd.com>
2016-05-31 21:42:08 +1000

    TC-16 Fix responsiveness of dismissible alert
    Fix responsiveness of dismissible alert using javascript

--- widgets/views/emailNotifications.php
+++ widgets/views/emailNotifications.php
@@ -5,28 +5,37 @@
     $cs = Yii::app()->getClientScript();
     $cs->registerScriptFile("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
 ?>
-<style>
-    .topbar {
-        margin-top: 51px; //need if alert not close
-    }
-</style>
-<div class="alert alert-info alert-message text-center">
+
+<div class="alert alert-info alert-message text-center" id="secondaryemail-alert">
     <center>Hi <?=  Yii::app()->user->getModel()->username; ?>, don't forget to <a href="<?= Yii::app()->createUrl('user/account/changeEmail') ?>">add a secondary email address</a> to your account to retain access to TeachConnect when your institutional email address expires.</center>
     <button type="button" class="close"><i class="fa fa-close"></i></button>    
 </div>
 
 <script>
     $(document).ready(function(){
-        var alert = $(".alert-message").clone();
+        var alertmessage = $(".alert-message").clone();
         $(".alert-message").remove();
-        $("#topbar-first").before('<div class="alert alert-info alert-message">' + alert.html() + '</div>');
+        $("#topbar-first").before('<div class="alert alert-info alert-message" id="secondaryemail-alert">' + alertmessage.html() + '</div>');
         $(".alert-message").animate({"opacity":1},1300);
-        
+        $(".topbar").animate({"marginTop" : 51}, 1000);
+        $("body > .container").animate({"marginTop" : 51}, 1000);
+        document.getElementById("topbar-first").className += " topbar-alert-margin";
+        document.getElementById("topbar-second").className += " topbar-alert-margin";
+
+            if(document.getElementById("secondaryemail-alert")){
+                document.getElementsByTagName('body')[0].className+=' topbar-alert-body'
+            }
+
         $(".alert-message .close").on("click", function(){
             $(".alert-message").fadeOut(1000);
             $(".topbar").animate({"marginTop" : 0}, 1000);
             $("body > .container").animate({"marginTop" : 0}, 1000);
             setCookie("secondery_email", 1);
+
+            document.getElementById("topbar-first").className =
+                document.getElementById("topbar-first").className.replace(/\btopbar-alert-margin\b/,'');
+            document.getElementById("topbar-second").className =
+                document.getElementById("topbar-second").className.replace(/\btopbar-alert-margin\b/,'');
         });
         
         function setCookie(key, value) {
