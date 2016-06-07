c076066f8c2ddc6d7be4e0793d1e890d64ccabb1
Den Volkov <volkovdenya@gmail.com>
2016-06-07 15:13:42 +0300

    TC-10 fix posting in post

--- widgets/views/emailNotifications.php
+++ widgets/views/emailNotifications.php
@@ -2,17 +2,17 @@
     if(isset(Yii::app()->user->getModel()->secondery_email) && empty(CHtml::encode(Yii::app()->user->getModel()->secondery_email)) && !isset($_COOKIE['secondery_email'])) {
 ?>
 <?php
-    $cs = Yii::app()->getClientScript();
-    $cs->registerScriptFile("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
+    //$cs = Yii::app()->getClientScript(); //disable write post
+    //$cs->registerScriptFile("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js");
 ?>
 
 <div class="alert alert-info alert-message text-center" id="secondaryemail-alert">
     <center>Hi <?=  Yii::app()->user->getModel()->username; ?>, don't forget to <a href="<?= Yii::app()->createUrl('user/account/changeEmail') ?>">add a secondary email address</a> to your account to retain access to TeachConnect when your institutional email address expires.</center>
-    <button type="button" class="close"><i class="fa fa-close"></i></button>    
+    <button type="button" class="close"><i class="fa fa-close"></i></button>
 </div>
 
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         var alertmessage = $(".alert-message").clone();
         $(".alert-message").remove();
         $("#topbar-first").before('<div class="alert alert-info alert-message" id="secondaryemail-alert">' + alertmessage.html() + '</div>');
@@ -37,7 +37,7 @@
             document.getElementById("topbar-second").className =
                 document.getElementById("topbar-second").className.replace(/\btopbar-alert-margin\b/,'');
         });
-        
+
         function setCookie(key, value) {
             var expires = new Date();
             expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
